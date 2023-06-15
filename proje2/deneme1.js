f00c6d4c6e9f88e687b20b9420e310c677618a57
Mert Namıduru <namidurumert@gmail.com>
2023-06-16 02:14:53 +0300

    Last_changes_v06.16

--- proje2/deneme1.js
+++ proje2/deneme1.js
@@ -24,8 +24,8 @@ document.getElementById('back-to-top').addEventListener('click', function() {
 
 document.getElementById('whatsapp-icon').addEventListener('click', function() {
     // WhatsApp'a mesaj göndermek için uygun bir URL oluşturun
-    var message = encodeURIComponent("Merhaba, size bir mesaj göndermek istiyorum.");
-    var whatsappUrl = "https://wa.me/905412631971?text=Merhabalar benim ismim Mert" + message;
+    var message = encodeURIComponent("Merhaba, bilgi almak istiyorum.Yardımcı olursanız çok sevinirim.Teşekkürler...");
+    var whatsappUrl = "https://wa.me/905412631971?text=" + message;
     
     // Oluşturulan URL'i yeni bir sekmede açın
     window.open(whatsappUrl, "_blank");
